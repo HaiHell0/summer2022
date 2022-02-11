9acd180195c6ecf4b5feed2e97328a1101dcaff2
Hai <hoangh1@nku.edu>
2022-02-11 16:36:42 -0500

    ok

---
+++ ASE220/Assignment3/dogArray.js
@@ -0,0 +1,65 @@
+const dogs = [
+    {   
+        Name:"Benny's",
+        Type:"Dog", 
+        Breed:"Australian Shephard Cross", 
+        Second_Breed:false,
+        Sex:"Male",
+        Color:"Brown/White",
+        Spayed_Neutered:true,
+        Age: {
+            "Years":2,
+            "Months":1,
+            "Weeks":4,
+            "approx":false
+            },
+        Details:{
+            Health:true,
+            Vacc:true,
+            Worming: true,
+            Microchpped: true
+        }
+    },
+    {   
+        Name:"Benny's",
+        Type:"Dog", 
+        Breed:"Australian Shephard Cross", 
+        Second_Breed:false,
+        Sex:"Male",
+        Color:"Brown/White",
+        Spayed_Neutered:true,
+        Age: {
+            "Years":2,
+            "Months":1,
+            "Weeks":4,
+            "approx":false
+            },
+        Details:{
+            Health:true,
+            Vacc:true,
+            Worming: true,
+            Microchpped: true
+        }
+    },
+    {   
+        Name:"Benny's",
+        Type:"Dog", 
+        Breed:"Australian Shephard Cross", 
+        Second_Breed:false,
+        Sex:"Male",
+        Color:"Brown/White",
+        Spayed_Neutered:true,
+        Age: {
+            "Years":2,
+            "Months":1,
+            "Weeks":4,
+            "approx":false
+            },
+        Details:{
+            Health:true,
+            Vacc:true,
+            Worming: true,
+            Microchpped: true
+        }
+    },
+]
